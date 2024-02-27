04ce9d232c8227bc9d57308215e258e3a8d31935
Haymos <amoz4christ@gmail.com>
2024-02-27 23:50:56 +0000

    testdb added

--- src/test/java/com/bezkoder/springjwt/FoodBackingBackendApplicationTests.java
+++ src/test/java/com/bezkoder/springjwt/FoodBackingBackendApplicationTests.java
@@ -1,16 +1,26 @@
 package com.bezkoder.springjwt;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.assertThat;
 
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class FoodBackingBackendApplicationTests {
 
 	@Test
 	public void contextLoads() {
+
 	}
 
+	@Test
+	public void runTest(){
+		assertThat("A").isEqualTo("A");
+	}
 }
